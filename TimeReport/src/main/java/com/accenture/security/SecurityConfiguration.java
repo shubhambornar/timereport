923325c14009b66efc72b5ddbecb71ac34595367
friday <friday@DESKTOP-TBQJUHQ>
2020-02-27 19:24:21 +0530

    security file commited by shubham

---
+++ TimeReport/src/main/java/com/accenture/security/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.accenture.security;
+
+
+import org.springframework.context.annotation.Configuration;  
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;  
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;  
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration  
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {  
+	@Override  
+	public void configure(HttpSecurity http) throws Exception {  
+		http  
+		.authorizeRequests()  
+		.anyRequest().authenticated()  
+		.and()  
+        .formLogin() 
+		.and()  
+		.httpBasic();  
+	}  
+	@Override  
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {  
+		auth.inMemoryAuthentication()  
+		.withUser("admin")  
+		.password("{noop}tiger")  
+		.roles("USER");  
+	}  
+}
